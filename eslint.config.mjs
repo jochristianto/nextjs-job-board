ba6ed87a7297314da3c20731a8bb9e3cd3155ff5
Jonathan Christianto <jo.christianto@gmail.com>
2025-05-29 19:16:21 +0700

    chore: setup vscode config, update eslint config, setup prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +11,40 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    // plugins: ["unused-imports", "no-relative-import-paths"],
+    plugins: {
+      "unused-imports": "unused-imports",
+      "no-relative-import-paths": "no-relative-import-paths",
+    },
+    rules: {
+      "react/no-unescaped-entities": "off",
+
+      // TypeScript
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/ban-types": "off",
+
+      // no-relative-import-paths
+      "no-relative-import-paths/no-relative-import-paths": [
+        "error",
+        { allowSameFolder: false, prefix: "@", rootDir: "src" },
+      ],
+
+      "@stylistic/js/max-len": [
+        "error",
+        {
+          code: 100,
+          comments: 150,
+          ignoreComments: true,
+          // ignoreTrailingComments: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+        },
+      ],
+      "@stylistic/js/comma-dangle": ["error", "always-multiline"],
+    },
+  },
 ];
 
 export default eslintConfig;
