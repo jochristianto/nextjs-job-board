d64a68a21e4aab7c1c4e0d8ae7031332c21a196d
Jonathan Christianto <jo.christianto@gmail.com>
2025-05-30 17:12:40 +0700

    feat: integrate shadcn sidebar

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -1,16 +1,30 @@
-import Footer from "@/components/footer";
-import Header from "@/components/header";
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
 export default function ProtectedLayout({ children }: { children: React.ReactNode }) {
+  // return (
+  //   <main className="min-h-screen flex flex-col items-center">
+  //     <div className="flex-1 w-full flex flex-col gap-20 items-center">
+  //       <Header />
+
+  //       <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">{children}</div>
+
+  //       <Footer />
+  //     </div>
+  //   </main>
+  // );
+
   return (
-    <main className="min-h-screen flex flex-col items-center">
-      <div className="flex-1 w-full flex flex-col gap-20 items-center">
-        <Header />
-
-        <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">{children}</div>
-
-        <Footer />
-      </div>
-    </main>
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "calc(var(--spacing) * 72)",
+          "--header-height": "calc(var(--spacing) * 12)"
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant="inset" />
+      <SidebarInset>{children}</SidebarInset>
+    </SidebarProvider>
   );
 }
