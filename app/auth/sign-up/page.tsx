3486a440d72411526d81c876f74cccc9ce140c1c
Jonathan Christianto <jo.christianto@gmail.com>
2025-05-31 20:08:42 +0700

    feat: add back button for all auth related pages

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,8 +1,18 @@
+"use client";
+
 import { SignUpForm } from "@/components/sign-up-form";
+import { Button } from "@/components/ui/button";
+import { ChevronLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
+  const router = useRouter();
+
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 relative">
+      <Button variant="outline" onClick={() => router.back()} className="absolute top-6 left-6">
+        <ChevronLeft className="size-4" />
+      </Button>
       <div className="w-full max-w-sm">
         <SignUpForm />
       </div>
